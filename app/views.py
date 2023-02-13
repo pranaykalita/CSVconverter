8fc55ddeaedf20443403bbd2a167ece6e8895612
pranaykalita <pranaykalita2@gmail.com>
2023-02-13 23:40:04 +0530

    Convert csv to xls

---
+++ app/views.py
@@ -0,0 +1,14 @@
+import pandas as pd
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+# Create your views here.
+def conv_app(request):
+    if request.method == 'POST':
+        upload_file = request.FILES.get('filefield')
+        df_name = 'converted.xls'
+        df = pd.read_csv(upload_file)
+        newdf = df.to_excel(df_name,index=False)
+    
+    return render(request , 'convapp.html')
